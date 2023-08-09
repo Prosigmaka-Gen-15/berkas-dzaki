c1581c5fbf68dc9148979a7fe14b7bf73bc58c3c
Ahmad  Dzaki Putra Dimas <ahmaddzakipd@gmail.com>
2023-08-09 09:16:29 +0700

    Upadate JS basic

---
+++ gen-23.05-javascript-basic/dist/main.js
@@ -0,0 +1,8 @@
+const mainImage = document.getElementById("main-img");
+const imageGrid = document.getElementById("change-img");
+imageGrid.addEventListener("click", (event) => {
+  if (event.target.tagName === "IMG") {
+    const clickedImageUrl = event.target.getAttribute("src");
+    mainImage.querySelector("img").setAttribute("src", clickedImageUrl);
+  }
+});
