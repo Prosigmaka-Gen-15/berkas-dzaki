548c2ef31309d7f82618bc3a8dd95f58606238d9
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-08-10 14:23:04 +0700

    fix: ubah metode ganti gambar

--- gen-23.05-javascript-basic/dist/main.js
+++ gen-23.05-javascript-basic/dist/main.js
@@ -1,8 +1,13 @@
-const mainImage = document.getElementById("main-img");
-const imageGrid = document.getElementById("change-img");
-imageGrid.addEventListener("click", (event) => {
-  if (event.target.tagName === "IMG") {
-    const clickedImageUrl = event.target.getAttribute("src");
-    mainImage.querySelector("img").setAttribute("src", clickedImageUrl);
-  }
-});
+function changeImage (gambar) {
+  const mainImage = document.getElementById("main-img-preview")
+  mainImage.src = gambar
+}
+
+// const mainImage = document.getElementById("main-img");
+// const imageGrid = document.getElementById("change-img");
+// imageGrid.addEventListener("click", (event) => {
+//   if (event.target.tagName === "IMG") {
+//     const clickedImageUrl = event.target.getAttribute("src");
+//     mainImage.querySelector("img").setAttribute("src", clickedImageUrl);
+//   }
+// });
